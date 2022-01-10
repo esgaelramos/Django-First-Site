4e8836df13dbee1b1c7f5d8f2a1f43b8d0430897
esgaelramos <esgaelramos@gmail.com>
2022-01-10 13:04:21 -0600

    Create example module, register on apps, recreate database now with ; and all migrate

--- Aplications/Manager/models.py
+++ Aplications/Manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Example(models.Model):
+    code = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "{0}".format(self.name)
